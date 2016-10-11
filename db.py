f9a4b163cc9704e673158c271574eca5c5a674d1
Brunston Poon <albatrossian@outlook.com>
2016-10-11 13:42:10 -0700

    first outline

---
+++ db.py
@@ -0,0 +1,35 @@
+## BPDB ##
+# brunston (c) 2016 or in perpetuity
+
+import numpy as np
+
+class BPDB:
+    """
+    A new BPDB object, which contains both structured and unstructured data.
+    Access this data with clean syntax.
+    >>> b = BPDB()
+    >>> b
+    BPDB()
+    >>>foo = [100]
+    >>> b.add_data(foo, 's')
+    >>>b
+    BPDB(1,0)
+    >>>bar = ['bar]
+    >>>b.add_data(bar, 'u')
+    >>>b
+    BPDB(1,1)
+    """
+    
+    def __init__(self):
+        self.u = np.array([])
+        self.s = np.array([])
+
+    def __repr(self):
+        return None
+
+    def add_data(self, data, dtype):
+        return None
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
