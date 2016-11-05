b43f069f137f9c1899d790fd2414e75800aa8cae
Brunston Poon <albatrossian@outlook.com>
2016-11-04 23:49:32 -0700

    testing stuff

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 ## BPDB ##
-# brunston (c) 2016 or in perpetuity
+# brunston (c) 2016
 
 import numpy as np
 
@@ -10,13 +10,13 @@ class BPDB:
     >>> b = BPDB()
     >>> b
     BPDB()
-    >>>foo = [100]
+    >>> foo = [100]
     >>> b.add_data(foo, 's')
-    >>>b
+    >>> b
     BPDB(1,0)
-    >>>bar = ['bar]
-    >>>b.add_data(bar, 'u')
-    >>>b
+    >>> bar = ['bar']
+    >>> b.add_data(bar, 'u')
+    >>> b
     BPDB(1,1)
     """
     
@@ -24,8 +24,11 @@ class BPDB:
         self.u = np.array([])
         self.s = np.array([])
 
-    def __repr(self):
-        return None
+    def __repr__(self):
+        return "BPDB"
+
+    def __str__(self):
+        return "BPDB"
 
     def add_data(self, data, dtype):
         return None
