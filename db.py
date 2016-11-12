735bb64643babfd2a614fefcf400721768cbf828
Brunston Poon <albatrossian@outlook.com>
2016-11-12 15:13:25 -0800

    flat file databases, adding

--- db.py
+++ db.py
@@ -8,30 +8,50 @@ class BPDB:
     A new BPDB object, which contains both structured and unstructured data.
     Access this data with clean syntax.
     >>> b = BPDB()
-    >>> b
-    BPDB()
+    >>> print(b)
+    BPDB(0,0)
     >>> foo = [100]
     >>> b.add_data(foo, 's')
-    >>> b
+    >>> print(b)
     BPDB(1,0)
     >>> bar = ['bar']
     >>> b.add_data(bar, 'u')
-    >>> b
+    >>> print(b)
     BPDB(1,1)
     """
     
-    def __init__(self):
-        self.u = np.array([])
-        self.s = np.array([])
-
-    def __repr__(self):
-        return "BPDB"
+    def __init__(self, name):
+        self.s = np.array([]) # structured
+        self.u = np.array([]) # unstructured
 
     def __str__(self):
-        return "BPDB"
+        return "BPDB({0},{1})".format(len(self.s), len(self.u))
 
     def add_data(self, data, dtype):
-        return None
+        """
+        adds data to the in-memory database. Does *not* modify flatfile databse
+        """
+        if dtype == "s":
+            self.s = np.append(self.s, data)
+        elif dtype == "u":
+            self.u = np.append(self.u, data)
+        else:
+            raise Exception('DTypeError')
+        return True # success
+
+    def syncout(self):
+        """
+        syncs in-memory database out to flatfile database
+        """
+        np.savetxt(name+"s.bpdb", self.s) # save structured
+        np.savetxt(name+"u.bpdb", self.u) # save unstructured
+
+        return
+
+    def syncin(self):
+        """
+        syncs flatfile database into memory.
+        """
 
 if __name__ == "__main__":
     import doctest
---
+++ test.py
@@ -0,0 +1,7 @@
+from db import *
+
+a = BPDB("blah")
+print(a)
+a.add_data("book", "s")
+print(a)
+print(a.s)
